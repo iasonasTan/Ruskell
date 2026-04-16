32a2a3090165e0fdc087fae7f1002e1e2e08154f
iasonas <iasonas.tan@gmail.com>
2026-04-16 04:06:36 +0300

    Added target

--- game.hs
+++ game.hs
@@ -9,31 +9,33 @@ reset  = "\ESC[0m"
 w = 20
 h = 10
 
-drawP px py x y = do
+drawP px py tx ty x y = do
   let c = if px == x && py == y 
           then green ++ "X" ++ reset
           else if isWall x y 
           then red ++ "$" ++ reset
+          else if tx == x && ty == y
+          then yellow ++ "#" ++ reset
           else blue ++  "*" ++ reset
   putStr c
 
-drawY px py x y = do
+drawY px py tx ty x y = do
   if x <= w then do
-    drawP px py x y
-    drawY px py (x+1) y
+    drawP px py tx ty x y
+    drawY px py tx ty (x+1) y
   else 
     putStrLn ""
 
-draw px py x y = do
+draw px py tx ty x y = do
   if y <= h then do
-    drawY px py x y
-    draw px py x (y+1)
+    drawY px py tx ty x y
+    draw px py tx ty x (y+1)
   else
     return ()
 
 isWall x y = x == 0 || y == 0 || x == w || y == h
 
-game opx opy = do
+game opx opy otx oty = do
   d <- getChar
   if d /= 'q' then do
     let npx = if d == 'a' && not (isWall (opx-1) opy) then opx - 1
@@ -43,8 +45,8 @@ game opx opy = do
               else if d == 's' && not (isWall opx (opy+1)) then opy + 1
               else opy
     putStr "\ESC[2J\ESC[H"
-    draw npx npy 0 0
-    game npx npy
+    draw npx npy otx oty 0 0
+    game npx npy otx oty
   else
     return ()
 
@@ -53,4 +55,4 @@ main = do
   hSetEcho stdout False
   putStrLn "Press any key to start."
   putStrLn "Move with WASD keys"
-  game 2 2
+  game 2 2 1 1
