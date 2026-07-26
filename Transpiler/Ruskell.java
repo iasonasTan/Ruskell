437d71d36cd50de7e93da5ab373218e2d5f59de4
iasonas <iasonas.tan@gmail.com>
2026-07-26 17:58:10 +0300

    Updated java transpiler

--- Transpiler/Ruskell.java
+++ Transpiler/Ruskell.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-// TODO: Rewrite in rust or d (seriously)
-
 public class Ruskell {
     public static void main(String[] args) {
         if (args.length < 2) {
@@ -10,71 +8,91 @@ public class Ruskell {
             return;
         }
 
-        String input  = args[0];
-        String output = args[1];
-        StringBuilder codeBuilder = new StringBuilder();
+        File input  = new File(args[0]);
+        File output = new File(args[1]);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(input)))) {
-            String ruskellLine;
-            while ((ruskellLine = reader.readLine()) != null) {
-                codeBuilder.append(parseLine(ruskellLine)).append("\n");
-            }
+        try (Transpiler transpiler = new Transpiler(output)) {
+            transpiler.transpile(input);
         } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(output)))) {
-            writer.write(codeBuilder.toString());
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            throw new UncheckedIOException(ioe);
         }
 
         System.out.println("Ruskell: Transpilation done!");
     }
 
-    public static String parseLine(String line) {
-        if (!line.contains("циτ")) {
-            return parseCode(line);
+    private static final class Transpiler implements Closeable {
+        private final StringBuilder codeBuilder = new StringBuilder();
+        private final File output;
+
+        Transpiler(File output) {
+            this.output = output;
+            if(!output.exists() || !output.isFile()) {
+                var exception = new IOException("Fatal Error: Given output file does not exist or it's a directory.");
+                throw new UncheckedIOException(exception);
+            }
         }
 
-        int start = line.indexOf("циτ");
-        int end = line.indexOf("конциτ");
+        public void transpile(File input) throws IOException {
+            try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
+                String ruskellLine;
+                while ((ruskellLine = reader.readLine()) != null) {
+                    codeBuilder.append(parseLine(ruskellLine)).append("\n");
+                }
+            }
+        }
 
-        if (end == -1) return parseCode(line);
+        private String parseLine(String line) {
+            final String QUOTES_OPEN = "циτ";
+            final String QUOTES_CLOSE= "конциτ";
 
-        String before = line.substring(0, start);
-        String inside = line.substring(start + 3, end);
-        String after = line.substring(end + 6);
-        return parseCode(before) + "\"" + inside + "\"" + parseLine(after);
+            if (!line.contains(QUOTES_OPEN) || !line.contains(QUOTES_CLOSE)) {
+                return parseCode(line);
+            }
+
+            int start = line.indexOf(QUOTES_OPEN);
+            int end = line.indexOf(QUOTES_CLOSE);
+
+            String before = line.substring(0, start);
+            String inside = line.substring(start+QUOTES_OPEN.length()+1, end-1); // +-1 is for spaces before/after циτ/конциτ
+            String after = line.substring(end+QUOTES_CLOSE.length());
+            return parseCode(before) + "\"" + inside + "\"" + parseLine(after);
+        }
+
+        private String parseCode(String line) {
+            line += " "; // Append space so `replace` catches even keywords in the end of the line
+            return line
+                .replace(" импорт ", " import ")
+                .replace(" открыτο ", " hiding ")
+                .replace(" является ", " :: ")
+                .replace(" равно ", " = ")
+                .replace(" делать ", " do ")
+                .replace(" пусть ", " let ")
+                .replace(" если ", " if ")
+                .replace(" тогда ", " then ")
+                .replace(" иначе ", " else ")
+                .replace(" вывестиСтроку ", " putStrLn ")
+                .replace(" вывести ", " putStr ")
+                .replace(" плюс ", " ++ ")
+                .replace(" минус ", " - ")
+                .replace(" меньшеРавно ", " <= ")
+                .replace(" стрелка ", " <- ")
+                .replace(" и ", " && ")
+                .replace(" или ", " || ")
+                .replace(" нет ", " not ")
+                .replace("главная ", "main ") // Main function will always be typed in the start of the line
+                .replace(" вернуть ", " return ")
+                .replace(" умножать ", " * ")
+                .replace(" покажи ", " print ")
+                .replace(" конско ", " ) ")
+                .replace(" равный ", " == ")
+                .replace(" ско ", " ( ");
+        }
+
+        @Override
+        public void close() throws IOException {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
+                writer.write(codeBuilder.toString());
+            }
+        }
     }
-
-    public static String parseCode(String line) {
-        line += " ";
-        return line
-            .replace(" импорт ", " import ")
-            .replace(" открыτο ", " hiding ")
-            .replace(" является ", " :: ")
-            .replace(" равно ", " = ")
-            .replace(" делать ", " do ")
-            .replace(" пусть ", " let ")
-            .replace(" если ", " if ")
-            .replace(" тогда ", " then ")
-            .replace(" иначе ", " else ")
-            .replace(" вывестиСтроку ", " putStrLn ")
-            .replace(" вывести ", " putStr ")
-            .replace(" плюс ", " ++ ")
-            .replace(" минус ", " - ")
-            .replace(" меньшеРавно ", " <= ")
-            .replace(" стрелка ", " <- ")
-            .replace(" и ", " && ")
-            .replace(" или ", " || ")
-            .replace(" нет ", " not ")
-            .replace("главная ", "main ") // Main function will always be typed in the start of the line
-            .replace(" вернуть ", " return ")
-            .replace(" умножать ", " * ")
-            .replace(" покажи ", " print ")
-            .replace(" конско ", " ) ")
-            .replace(" равный ", " == ")
-            .replace(" ско ", " ( ");
-    }    
 }
